34955d75f230e43ece63ce1945f19799a38bbcbe
Eric Collins <tabfugnic@gmail.com>
2014-01-09 16:29:21 -0500

    added notes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Bookshelf::Application.routes.draw do
   root "books#index"
   resources :books
 
+  resources :notes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
